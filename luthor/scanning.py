734683d13f6d611e98105fae30a54e35cde45524
eric-hermosis <eric.hermosis@gmail.com>
2026-03-11 18:34:34 -0300

    Redesign

--- luthor/scanning.py
+++ luthor/scanning.py
@@ -1,19 +1,2 @@
 from dataclasses import dataclass
-from typing import Iterator, Generator 
-from luthor.lexing import Lexer
-
-@dataclass(frozen=True)
-class Token:
-    name : str
-    value: str  
-
-
-class Scanner:
-
-    def __init__(self, lexer: Lexer) -> None:
-        self.lexer = lexer
-    
-    def scan(self, stream: Iterator[str]) -> Generator[Token, None, None]:
-        for line in stream:
-            for lexeme in self.lexer.analyze(line):
-                yield Token(lexeme.lemma, ''.join(lexeme.words))
+from typing import Iterator, Generator  
--- main.py
+++ main.py
@@ -1,17 +1,82 @@
-from luthor.lexing import Lexer, Lexicon, Rule, Feature
-from luthor.scanning import Scanner
-from re import compile
-from io import StringIO
+from typing import Generator
+from typing import Sequence, List 
+from re import Pattern, Match
 
-equation = StringIO(r"""
+class Lexeme:
+    lemma: str 
+    match: str | Match[str]
+
+    def __init__(self, lemma: str, match: str | Match[str]):
+        self.lemma = lemma
+        self.match = match
+
+class Rule:
+    name: str 
+
+    def __init__(self, pattern: Pattern[str], name: str):
+        self.pattern = pattern
+        self.name    = name 
+
+    def search(self, chunk: str) -> Lexeme:
+        match = self.pattern.search(chunk)
+        return Lexeme(self.name, match) if match else None
+
+    def match(self, chunk: str, position: int) -> Lexeme | None:
+        match = self.pattern.match(chunk, position)
+        return Lexeme(self.name, match) if match else None
+
+class Lexicon:
+    rules: List[Rule]
+
+    def __init__(self, rules: Sequence[Rule]):
+        self.rules = list(rules)
+        
+
+class Lexer:    
+    def __init__(self, lexicon: Lexicon) -> None:
+        self.lexicon = lexicon
+        self.buffer  = []  
+        self.state   = None
+
+    def flush(self) -> Generator[Lexeme, None, None]:
+        if self.buffer:
+            yield Lexeme(self.state or 'CONTENT', ''.join(self.buffer))
+            self.buffer.clear()
+
+    def analyze(self, chunk: str) -> Generator[Lexeme, None, None]:
+        position = 0
+        while position < len(chunk): 
+            for rule in self.lexicon.rules:
+                lexeme = rule.match(chunk, position)
+                if lexeme:
+                    yield from self.flush()
+                    yield lexeme
+                    position = lexeme.match.end()
+                    break
+                
+            else:
+                self.buffer.append(chunk[position])
+                position+=1
+        yield from self.flush()
+
+from io import StringIO
+from re import compile
+
+if __name__ == '__main__': 
+    lexicon = Lexicon([
+        Rule(compile(r'^#{1,6}'), 'HASHTAG'),
+        Rule(compile(r'\${1,2}'), 'DOLLARS'),
+        Rule(compile(r'\n$')    , 'NEWLINE')
+    ])
+
+    example = StringIO(r"""
+### Title
 $$
-E = mc^2
+w = x*y + y*z + z*x
 $$
-""")
-lexicon = Lexicon([
-    Rule(Feature('SIGN', 'MATH'), compile(r'\$+'))
-])
-lexer   = Lexer(lexicon)
-scanner = Scanner(lexer)
-for token in scanner.scan(equation):
-    print(token)
+"""
+)
+    lexer = Lexer(lexicon)
+    for line in example:
+        for lexeme in lexer.analyze(line):
+            print(lexeme.lemma, lexeme.match)
